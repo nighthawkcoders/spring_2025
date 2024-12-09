6397c3a5c4f81af9bd10e446bc9e27bf6a1c417b
spooketti <jonliu168@gmail.com>
2024-12-09 12:42:07 -0800

    I AM LOSING MY MIND I AM LOSING MY IND

--- src/main/java/com/nighthawk/spring_portfolio/mvc/mortevision/SFU.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/mortevision/SFU.java
@@ -1,8 +1,11 @@
 package com.nighthawk.spring_portfolio.mvc.mortevision;
 
 import java.util.Collections;
+import java.util.Map;
+
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +20,7 @@ import dev.onvoid.webrtc.RTCPeerConnection;
 import dev.onvoid.webrtc.RTCRtpReceiver;
 import dev.onvoid.webrtc.RTCSdpType;
 import dev.onvoid.webrtc.RTCSessionDescription;
+import dev.onvoid.webrtc.SetSessionDescriptionObserver;
 import dev.onvoid.webrtc.media.MediaStream;
 import dev.onvoid.webrtc.media.MediaStreamTrack;
 
@@ -50,28 +54,59 @@ public class SFU implements PeerConnectionObserver {
         return payload.toString();
     }
 
-    @PostMapping("/broadcast")
-    public String broadcast(String stp) {
+    @PostMapping(value="/broadcast",consumes ="application/json")
+    public String broadcast(@RequestBody String body) {
+        String sdp = new JSONObject(body).getJSONObject("sdp").getString("sdp");
         RTCIceServer iceServer = new RTCIceServer();
         iceServer.urls = Collections.singletonList("stun:stun.l.google.com:19302");
         RTCConfiguration config = new RTCConfiguration();
         config.iceServers = Collections.singletonList(iceServer);
+        System.out.println("I HATE PEOPLE I HATE PEOPLE I HATE PEOPLE");
         PeerConnectionFactory PCF = new PeerConnectionFactory();
+        System.out.println("FU");
         RTCPeerConnection peerConnection = PCF.createPeerConnection(config, this);
+        System.out.println("FUDGE");
+        System.out.println(sdp);
+        RTCSessionDescription sessionDescription = new RTCSessionDescription(RTCSdpType.OFFER, sdp);
+        peerConnection.setRemoteDescription(sessionDescription, new SetSessionDescriptionObserver() {
 
-        RTCSessionDescription sessionDescription = new RTCSessionDescription(RTCSdpType.OFFER, stp);
-        peerConnection.setRemoteDescription(sessionDescription, null);
+            @Override
+            public void onSuccess() {
+                // TODO Auto-generated method stub
+                // throw new UnsupportedOperationException("Unimplemented method 'onSuccess'");
+            }
+
+            @Override
+            public void onFailure(String error) {
+                // TODO Auto-generated method stub
+                // throw new UnsupportedOperationException("Unimplemented method 'onFailure'");
+            }
+            
+        });
+        System.out.println("kill myself arc");
 
         peerConnection.createAnswer(new RTCAnswerOptions(), new CreateSessionDescriptionObserver() {
 
             @Override
             public void onSuccess(RTCSessionDescription description) {
-                peerConnection.setLocalDescription(description, null);
+                peerConnection.setLocalDescription(description, new SetSessionDescriptionObserver() {
+
+                    @Override
+                    public void onSuccess() {
+                        // throw new UnsupportedOperationException("Unimplemented method 'onSuccess'");
+                    }
+
+                    @Override
+                    public void onFailure(String error) {
+                        // throw new UnsupportedOperationException("Unimplemented method 'onFailure'");
+                    }
+                    
+                });
             }
 
             @Override
             public void onFailure(String error) {
-                throw new UnsupportedOperationException("Unimplemented method 'onFailure'");
+                // throw new UnsupportedOperationException("Unimplemented method 'onFailure'");
             }
 
         });
@@ -86,6 +121,6 @@ public class SFU implements PeerConnectionObserver {
 
     @Override
     public void onIceCandidate(RTCIceCandidate candidate) {
-        throw new UnsupportedOperationException("Unimplemented method 'onIceCandidate'");
+        // throw new UnsupportedOperationException("Unimplemented method 'onIceCandidate'");
     }
 }
