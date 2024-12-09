821d4424efda7710c4df9f7859befc8907f271aa
spooketti <jonliu168@gmail.com>
2024-12-09 09:41:05 -0800

    post mappings that i will end up not using anyways because of websockets but i am honestly on my knees here i cant take it anymore

--- src/main/java/com/nighthawk/spring_portfolio/mvc/mortevision/SFU.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/mortevision/SFU.java
@@ -1,8 +1,15 @@
 package com.nighthawk.spring_portfolio.mvc.mortevision;
+
 import java.util.Collections;
 import org.json.JSONObject;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.onvoid.webrtc.CreateSessionDescriptionObserver;
 import dev.onvoid.webrtc.PeerConnectionFactory;
 import dev.onvoid.webrtc.PeerConnectionObserver;
+import dev.onvoid.webrtc.RTCAnswerOptions;
 import dev.onvoid.webrtc.RTCConfiguration;
 import dev.onvoid.webrtc.RTCIceCandidate;
 import dev.onvoid.webrtc.RTCIceServer;
@@ -13,63 +20,72 @@ import dev.onvoid.webrtc.RTCSessionDescription;
 import dev.onvoid.webrtc.media.MediaStream;
 import dev.onvoid.webrtc.media.MediaStreamTrack;
 
-public class SFU implements PeerConnectionObserver{
+@RestController
+@RequestMapping("/webrtc")
+public class SFU implements PeerConnectionObserver {
 
-MediaStream broadcaster;
+    MediaStream broadcaster;
 
-//see front end for complaints
+    @PostMapping("/consume")
+    public String consumer(String stp) {
+        if (broadcaster == null) {
+            return new JSONObject("{'error':'no broadcast'}").toString();
+        }
+        RTCIceServer iceServer = new RTCIceServer();
+        iceServer.urls = Collections.singletonList("stun:stun.l.google.com:19302");
 
-public void consumer(String stp)
-{
-    if(broadcaster == null)
-    {
-        return;
+        RTCConfiguration config = new RTCConfiguration();
+        config.iceServers = Collections.singletonList(iceServer);
+
+        PeerConnectionFactory PCF = new PeerConnectionFactory();
+        RTCPeerConnection peerConnection = PCF.createPeerConnection(config, null);
+
+        RTCSessionDescription sessionDescription = new RTCSessionDescription(RTCSdpType.OFFER, stp);
+        peerConnection.setRemoteDescription(sessionDescription, null); // this is most likley the problem
+        peerConnection.addTrack((MediaStreamTrack) broadcaster.getAudioTracks()[0], null);
+        peerConnection.addTrack((MediaStreamTrack) broadcaster.getVideoTracks()[0], null);
+        peerConnection.createAnswer(new RTCAnswerOptions(), null);
+        peerConnection.setLocalDescription(peerConnection.getLocalDescription(), null);
+        JSONObject payload = new JSONObject().put("sdp", peerConnection.getLocalDescription());
+        return payload.toString();
     }
-    RTCIceServer iceServer = new RTCIceServer();
-    iceServer.urls = Collections.singletonList("stun:stun.l.google.com:19302");
 
-    RTCConfiguration config = new RTCConfiguration(); 
-    config.iceServers = Collections.singletonList(iceServer);
+    @PostMapping("/broadcast")
+    public String broadcast(String stp) {
+        RTCIceServer iceServer = new RTCIceServer();
+        iceServer.urls = Collections.singletonList("stun:stun.l.google.com:19302");
+        RTCConfiguration config = new RTCConfiguration();
+        config.iceServers = Collections.singletonList(iceServer);
+        PeerConnectionFactory PCF = new PeerConnectionFactory();
+        RTCPeerConnection peerConnection = PCF.createPeerConnection(config, this);
 
-    PeerConnectionFactory PCF = new PeerConnectionFactory();
-    RTCPeerConnection peerConnection = PCF.createPeerConnection(config,null);
+        RTCSessionDescription sessionDescription = new RTCSessionDescription(RTCSdpType.OFFER, stp);
+        peerConnection.setRemoteDescription(sessionDescription, null);
 
-    RTCSessionDescription sessionDescription = new RTCSessionDescription(RTCSdpType.OFFER, stp);
-    peerConnection.setRemoteDescription(sessionDescription, null); //this is most likley the problem
-    peerConnection.addTrack((MediaStreamTrack)broadcaster.getAudioTracks()[0],null);
-    peerConnection.addTrack((MediaStreamTrack)broadcaster.getVideoTracks()[0], null);
-    peerConnection.createAnswer(null, null);
-    peerConnection.setLocalDescription(null, null);
-    
-    JSONObject payload = new JSONObject("{'sdp':" + peerConnection.getLocalDescription() + "}");
-}
+        peerConnection.createAnswer(new RTCAnswerOptions(), new CreateSessionDescriptionObserver() {
 
-public void broadcast(String stp)
-{
-    RTCIceServer iceServer = new RTCIceServer();
-    iceServer.urls = Collections.singletonList("stun:stun.l.google.com:19302");
-    RTCConfiguration config = new RTCConfiguration(); 
-    config.iceServers = Collections.singletonList(iceServer);
-    PeerConnectionFactory PCF = new PeerConnectionFactory();
-    RTCPeerConnection peerConnection = PCF.createPeerConnection(config,this);
+            @Override
+            public void onSuccess(RTCSessionDescription description) {
+                peerConnection.setLocalDescription(description, null);
+            }
 
-    RTCSessionDescription sessionDescription = new RTCSessionDescription(RTCSdpType.OFFER, stp);
-    peerConnection.setRemoteDescription(sessionDescription, null);
+            @Override
+            public void onFailure(String error) {
+                throw new UnsupportedOperationException("Unimplemented method 'onFailure'");
+            }
 
-    peerConnection.createAnswer(null, null);
-    peerConnection.setLocalDescription(sessionDescription, null);
+        });
+        JSONObject payload = new JSONObject().put("sdp", peerConnection.getLocalDescription());
+        return payload.toString();
+    }
 
-    JSONObject payload = new JSONObject("{'sdp':" + peerConnection.getLocalDescription() + "}");
-}
+    @Override
+    public void onAddTrack(RTCRtpReceiver receiver, MediaStream[] mediaStreams) {
+        broadcaster = mediaStreams[0];
+    }
 
-@Override
-public void onAddTrack(RTCRtpReceiver receiver, MediaStream[] mediaStreams) {
-    broadcaster = mediaStreams[0];
-}
-
-@Override
-public void onIceCandidate(RTCIceCandidate candidate) {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'onIceCandidate'");
-}
+    @Override
+    public void onIceCandidate(RTCIceCandidate candidate) {
+        throw new UnsupportedOperationException("Unimplemented method 'onIceCandidate'");
+    }
 }
