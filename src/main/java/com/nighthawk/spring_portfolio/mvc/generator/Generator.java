85271fd8040d1d8cdd44257e35a3fa69b041817e
iKAN2025 <142475176+iKAN2025@users.noreply.github.com>
2024-12-09 21:45:30 -0800

    get rid of unneeded annotation

--- src/main/java/com/nighthawk/spring_portfolio/mvc/generator/Generator.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/generator/Generator.java
@@ -1,17 +1,25 @@
 package com.nighthawk.spring_portfolio.mvc.generator;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.http.*;
-import org.springframework.web.client.RestTemplate;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 
-@SpringBootApplication
+
 @RestController
 @RequestMapping("/generate")
 @CrossOrigin(origins = "*")
