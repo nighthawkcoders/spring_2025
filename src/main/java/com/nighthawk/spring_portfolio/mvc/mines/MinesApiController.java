971d7e5c73912991b1148ec3a12ec89b5e4b9322
dino596 <arthurliutw@gmail.com>
2024-12-10 10:40:39 -0800

    added email implementation

--- src/main/java/com/nighthawk/spring_portfolio/mvc/mines/MinesApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/mines/MinesApiController.java
@@ -1,22 +1,47 @@
 package com.nighthawk.spring_portfolio.mvc.mines;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.nighthawk.spring_portfolio.mvc.person.Person;
+import com.nighthawk.spring_portfolio.mvc.person.PersonJpaRepository;
+
+import lombok.Getter;
+
 @RestController
 @RequestMapping("/api/casino/mines")
 public class MinesApiController {
-    MinesBoard board;
+    private MinesBoard board;
+
+    @Getter
+    public static class MinesRequest {
+        private double betSize;
+        private String email;
+    }
+
+    @Autowired
+    private PersonJpaRepository personJpaRepository;
 
     @GetMapping("/{xCoord}/{yCoord}")
     public ResponseEntity<Boolean> getMine(@PathVariable int xCoord, @PathVariable int yCoord) {
         return new ResponseEntity<>(board.checkMine(xCoord, yCoord), HttpStatus.OK);
     }
 
-    @GetMapping("/winnings")
-    public ResponseEntity<Double> getWinnings() {
-        return new ResponseEntity<>(board.winnings(), HttpStatus.OK);
+    @PostMapping("/winnings")
+    public ResponseEntity<Double> calculateWinnings(@RequestBody MinesRequest minesRequest) {
+        Person user = personJpaRepository.findByEmail(minesRequest.getEmail());
+
+        double betSize = minesRequest.getBetSize();
+        if (user.getBalance() < betSize) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        
+        user.setBalance(user.getBalance() - betSize + board.winnings() * betSize);
+
+        personJpaRepository.save(user);
+        return new ResponseEntity<>(user.getBalance(), HttpStatus.OK);
     }
 
     @PostMapping("/stakes/{stakes}")
