67af00b16a52a9820fbb1c5975418036f5b10f15
dino596 <arthurliutw@gmail.com>
2024-12-11 12:32:43 -0800

    added javadocs and lombok

--- src/main/java/com/nighthawk/spring_portfolio/mvc/mines/MinesApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/mines/MinesApiController.java
@@ -1,36 +1,45 @@
 package com.nighthawk.spring_portfolio.mvc.mines;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import com.nighthawk.spring_portfolio.mvc.person.Person;
 import com.nighthawk.spring_portfolio.mvc.person.PersonJpaRepository;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
-import lombok.Getter;
-
+@Slf4j
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/casino/mines")
 public class MinesApiController {
+    private final PersonJpaRepository personJpaRepository;
     private MinesBoard board;
 
-    @Getter
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class MinesRequest {
         private double betSize;
         private String email;
     }
 
-    @Autowired
-    private PersonJpaRepository personJpaRepository;
+    private boolean hasSufficientBalance(Person user, double betSize) {
+        if (user.getBalance() < betSize) {
+            log.warn("Insufficient balance for user {}. Bet size: {}, Balance: {}", user.getEmail(), betSize, user.getBalance());
+            return false;
+        }
+        return true;
+    }
 
     @GetMapping("/{xCoord}/{yCoord}")
     public ResponseEntity<Boolean> getMine(@PathVariable int xCoord, @PathVariable int yCoord) {
+        log.info("Checking mine at coordinates ({}, {})", xCoord, yCoord);
         return new ResponseEntity<>(board.checkMine(xCoord, yCoord), HttpStatus.OK);
     }
 
@@ -38,12 +47,11 @@ public class MinesApiController {
     public ResponseEntity<Double> calculateWinnings(@RequestBody MinesRequest minesRequest) {
         Person user = personJpaRepository.findByEmail(minesRequest.getEmail());
         double betSize = minesRequest.getBetSize();
-        if (user.getBalance() < betSize) {
+        if (!hasSufficientBalance(user, betSize)) {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-        
-        user.setBalance(user.getBalance() + board.winnings() * betSize);
 
+        user.setBalance(user.getBalance() + board.winnings() * betSize);
         personJpaRepository.save(user);
         return new ResponseEntity<>(user.getBalance(), HttpStatus.OK);
     }
@@ -54,26 +62,24 @@ public class MinesApiController {
         if (user == null) {
             return new ResponseEntity<>("User not found", HttpStatus.NOT_FOUND);
         }
-    
+
         double betSize = minesRequest.getBetSize();
-        if (user.getBalance() < betSize) {
+        if (!hasSufficientBalance(user, betSize)) {
             return new ResponseEntity<>("Insufficient balance", HttpStatus.BAD_REQUEST);
         }
-    
+
         user.setBalance(user.getBalance() - betSize);
         personJpaRepository.save(user);
-    
         board = new MinesBoard(stakes);
-    
         return new ResponseEntity<>(HttpStatus.OK);
     }
-    
+
     @GetMapping("/balance/{email}")
     public ResponseEntity<Double> getBalance(@PathVariable String email) {
-    Person user = personJpaRepository.findByEmail(email);
-    if (user == null) {
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-    }
-    return new ResponseEntity<>(user.getBalance(), HttpStatus.OK);
+        Person user = personJpaRepository.findByEmail(email);
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(user.getBalance(), HttpStatus.OK);
     }
 }
