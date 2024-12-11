1a736c7081bfc5f2fdbe58ec02d21deed87da601
M8tth3 <mw.mountaindragon@gmail.com>
2024-12-10 23:02:38 -0800

    queue fix

--- src/main/java/com/nighthawk/spring_portfolio/mvc/bathroom/BathroomQueue.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/bathroom/BathroomQueue.java
@@ -9,11 +9,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class BathroomQueue {
     @Id
