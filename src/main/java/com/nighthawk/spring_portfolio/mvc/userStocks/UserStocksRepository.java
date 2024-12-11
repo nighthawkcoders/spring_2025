9476284af890830db8dd9e2e2bb366642d3b3e4f
Aadit Mathur <62529127+aaguy-hue@users.noreply.github.com>
2024-12-10 22:05:12 -0800

    Revert "Gamify Backend Change"

--- src/main/java/com/nighthawk/spring_portfolio/mvc/userStocks/UserStocksRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nighthawk.spring_portfolio.mvc.userStocks;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserStocksRepository extends JpaRepository<userStocksTable, Long> {
-    // You can add custom methods if needed, but JpaRepository already provides save(), findById(), etc.
-}
