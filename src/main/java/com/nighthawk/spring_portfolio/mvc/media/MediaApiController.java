4abbe6adb52a7b41d4ae5c4e573494eaee973a6d
shuban-789 <cybergeek11929@gmail.com>
2024-12-11 12:56:46 -0800

    default is leaderboard

--- src/main/java/com/nighthawk/spring_portfolio/mvc/media/MediaApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/media/MediaApiController.java
@@ -32,11 +32,8 @@ public class MediaApiController {
     // Get all analytics records
     @GetMapping("/")
     public ResponseEntity<List<Scores>> getAllAnalytics() {
-        List<SynergyGrade> gradeList = gradeJpaRepository.findAll();  // Fetch all grade records from database
-        if (gradeList.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT); // No records found
-        }
-        return new ResponseEntity<>(gradeList, HttpStatus.OK); // Return found records
+        List<Integer> scores = mediaJpaRepository.findAll(); // Fetch leaderboard
+        return scores;  // Return list of assignment IDs
     }
 
     
--- src/main/java/com/nighthawk/spring_portfolio/mvc/media/Scores.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/media/Scores.java
@@ -1,22 +1,10 @@
 package com.nighthawk.spring_portfolio.mvc.media;
 
-import com.nighthawk.spring_portfolio.mvc.person.Person;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -37,6 +25,4 @@ public class Scores {
     private int uid;
     private String person_name;
     private int score;
-
-    
 }
