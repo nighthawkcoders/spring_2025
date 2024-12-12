26af73e43df980fec3564e02215a254c958611ee
KinetekEnergy <transyt8@gmail.com>
2024-12-11 19:33:51 -0800

    qol change

--- src/main/java/com/nighthawk/spring_portfolio/mvc/analytics/AnalyticsApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/analytics/AnalyticsApiController.java
@@ -10,7 +10,6 @@ import org.springframework.web.server.ResponseStatusException;
 
 import com.nighthawk.spring_portfolio.mvc.assignments.Assignment;
 import com.nighthawk.spring_portfolio.mvc.assignments.AssignmentJpaRepository;
-import com.nighthawk.spring_portfolio.mvc.person.Person;
 import com.nighthawk.spring_portfolio.mvc.synergy.SynergyGrade;
 import com.nighthawk.spring_portfolio.mvc.synergy.SynergyGradeJpaRepository;
 
--- src/main/java/com/nighthawk/spring_portfolio/mvc/bathroom/BathroomQueue.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/bathroom/BathroomQueue.java
@@ -28,29 +28,50 @@ public class BathroomQueue {
     private int away;
 
     // Custom constructor
+
+    /**
+     * Constructor which creates each element in the queue
+     * 
+     * @param teacherEmail - the teacher's email for what class they are from
+     * @param peopleQueue  - the people in the queue
+     */
     public BathroomQueue(String teacherEmail, String peopleQueue) {
         this.teacherEmail = teacherEmail;
         this.peopleQueue = peopleQueue;
         this.away = 0;
     }
-    
+
+    /**
+     * Function to add a student to the queue
+     * 
+     * @param studentName - the name you want to add to the queue
+     */
     public void addStudent(String studentName) {
         if (this.peopleQueue == null || this.peopleQueue.isEmpty()) {
             this.peopleQueue = studentName;
-        } 
-        else {
+        } else {
             this.peopleQueue += "," + studentName;
         }
     }
 
+    /**
+     * Function to remove the student from a queue
+     * 
+     * @param studentName - the name you want to remove from the queue. In frontend,
+     *                    your own name is passed.
+     */
     public void removeStudent(String studentName) {
         if (this.peopleQueue != null) {
             this.peopleQueue = Arrays.stream(this.peopleQueue.split(","))
-                .filter(s -> !s.equals(studentName))
-                .collect(Collectors.joining(","));
+                    .filter(s -> !s.equals(studentName))
+                    .collect(Collectors.joining(","));
         }
     }
 
+    /**
+     * @return - returns the student who is at the front of the line, removing the
+     *         commas and sanitizing the data
+     */
     public String getFrontStudent() {
         if (this.peopleQueue != null && !this.peopleQueue.isEmpty()) {
             return this.peopleQueue.split(",")[0];
@@ -58,6 +79,11 @@ public class BathroomQueue {
         return null;
     }
 
+    /**
+     * Students need to be approved to go to the bathroom by the teacher
+     * When they are, their status is set to away
+     * When they return, they are removed from the queue
+     */
     public void approveStudent() {
         if (this.peopleQueue != null && !this.peopleQueue.isEmpty()) {
             if (this.away == 0) {
@@ -77,8 +103,10 @@ public class BathroomQueue {
             throw new IllegalStateException("Queue is empty");
         }
     }
-    
 
+    /**
+     * @return - initialize the queue
+     */
     public static BathroomQueue[] init() {
         ArrayList<BathroomQueue> queues = new ArrayList<>();
         queues.add(new BathroomQueue("jm1021@gmail.com", ""));
