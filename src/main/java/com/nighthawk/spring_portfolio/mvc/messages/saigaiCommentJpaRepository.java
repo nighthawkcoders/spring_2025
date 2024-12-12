ce8eb0afdded0ed3bc73d5406788fda6e33d638c
Joshua Thinh <joshua.thinh@gmail.com>
2024-12-12 01:30:01 -0800

    renamed comments to fix same file name error.

--- src/main/java/com/nighthawk/spring_portfolio/mvc/messages/saigaiCommentJpaRepository.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/messages/saigaiCommentJpaRepository.java
@@ -3,5 +3,5 @@ package com.nighthawk.spring_portfolio.mvc.messages;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 // JPA is an object-relational mapping (ORM) to persistent data, originally relational databases (SQL). Today JPA implementations has been extended for NoSQL.
-public interface CommentJpaRepository extends JpaRepository<Comment, Long> {
+public interface saigaiCommentJpaRepository extends JpaRepository<saigaiComment, Long> {
 }
