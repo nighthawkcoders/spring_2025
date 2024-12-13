bab04452ad81d36b04e53ce744369f86aad382fb
aaguy-hue <aaditmathur6@gmail.com>
2024-12-13 11:50:20 -0800

    remove unused imports

--- src/main/java/com/nighthawk/spring_portfolio/mvc/synergy/SynergyApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/synergy/SynergyApiController.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,9 +23,7 @@ import com.nighthawk.spring_portfolio.mvc.person.Person;
 import com.nighthawk.spring_portfolio.mvc.person.PersonJpaRepository;
 
 import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @RestController
 @RequestMapping("/api/synergy")
