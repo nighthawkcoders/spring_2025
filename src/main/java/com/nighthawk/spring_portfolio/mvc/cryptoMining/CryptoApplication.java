2098ff966c65ec5a5129c73eb727ee4f1ad601bd
iKAN2025 <142475176+iKAN2025@users.noreply.github.com>
2024-12-18 09:37:46 -0800

    cryptomining

--- src/main/java/com/nighthawk/spring_portfolio/mvc/cryptoMining/CryptoApplication.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/cryptoMining/CryptoApplication.java
@@ -1,11 +1,10 @@
 package com.nighthawk.spring_portfolio.mvc.cryptoMining;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+
 public class CryptoApplication {
 
     public static void main(String[] args) {
