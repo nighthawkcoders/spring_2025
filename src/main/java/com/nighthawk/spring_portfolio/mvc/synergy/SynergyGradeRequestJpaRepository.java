3a90709c56374f3514a63400d1913e53b38b4af0
iKAN2025 <142475176+iKAN2025@users.noreply.github.com>
2025-01-08 15:07:26 -0800

    Revert "Revert "Teacher toolkit Updating the AI Grader link""

--- src/main/java/com/nighthawk/spring_portfolio/mvc/synergy/SynergyGradeRequestJpaRepository.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/synergy/SynergyGradeRequestJpaRepository.java
@@ -1,9 +1,12 @@
 package com.nighthawk.spring_portfolio.mvc.synergy;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SynergyGradeRequestJpaRepository extends JpaRepository<SynergyGradeRequest, Long> {
+    List<SynergyGradeRequest> findByStudentId(Long studentId);
 
 }
