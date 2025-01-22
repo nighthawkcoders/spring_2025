0a2f2f3a6cfa7f26db2033bbe8675a15d04676ce
Aditya Samavedam <adityasam1357@gmail.com>
2025-01-22 11:25:23 -0800

    period 3 tables page created, view controller

--- src/main/java/com/nighthawk/spring_portfolio/mvc/student/StudentInfoViewController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/student/StudentInfoViewController.java
@@ -1,28 +1,21 @@
 package com.nighthawk.spring_portfolio.mvc.student;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.server.ResponseStatusException;
-
-import com.nighthawk.spring_portfolio.mvc.person.Person;
-import com.nighthawk.spring_portfolio.mvc.person.PersonJpaRepository;
 
 @Controller
 @RequestMapping("/mvc/student")
 public class StudentInfoViewController {
+
+    @GetMapping("/tableper1")
+    public String viewTablesPer1() {
+        return "student/tables_per1.html";
+    }
     
-    @GetMapping("/tables")
-    public String viewTables() {
-        return "student/tables.html";
+    @GetMapping("/tablesper3")
+    public String viewTablesPer3() {
+        return "student/tables_per3.html";
     }
 
 }
