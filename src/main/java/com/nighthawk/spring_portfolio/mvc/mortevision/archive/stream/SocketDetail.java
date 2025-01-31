425a91fd4a49c5cbb14dcc92ae2674a46a1b8659
aaguy-hue <aaditmathur6@gmail.com>
2025-01-31 00:16:55 -0800

    add back mortevision

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/mortevision/archive/stream/SocketDetail.java
@@ -0,0 +1,13 @@
+package com.nighthawk.spring_portfolio.mvc.mortevision.archive.stream;
+// package com.nighthawk.spring_portfolio.mvc.mortevision.stream;
+
+// import lombok.AllArgsConstructor;
+// import lombok.Data;
+// import lombok.NoArgsConstructor;
+
+// @Data
+// @NoArgsConstructor
+// @AllArgsConstructor
+// public class SocketDetail {
+//     private String name;
+// }
