06f4b3951d4cd2edbff6676b80e94762ad6f5c89
Joshua Thinh <joshua.thinh@gmail.com>
2025-02-04 09:22:10 -0800

    fixed messages name

--- src/main/java/com/nighthawk/spring_portfolio/mvc/messages/SagaiMessage.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/messages/SagaiMessage.java
@@ -20,10 +20,10 @@ import jakarta.persistence.*;
 @Data  // Annotations to simplify writing code (ie constructors, setters)
 @NoArgsConstructor
 @AllArgsConstructor
-public class SaigaiMessage implements Comparable<SaigaiMessage> {
+public class SagaiMessage implements Comparable<SagaiMessage> {
 
     
-    private static final Logger logger = LoggerFactory.getLogger(SaigaiMessage.class);
+    private static final Logger logger = LoggerFactory.getLogger(SagaiMessage.class);
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -35,10 +35,10 @@ public class SaigaiMessage implements Comparable<SaigaiMessage> {
 
     @JsonManagedReference
     @OneToMany(mappedBy = "message", cascade = CascadeType.REMOVE, orphanRemoval = true, fetch = FetchType.EAGER)
-    private List<SaigaiComment> comments = new ArrayList<>();
+    private List<SagaiComment> comments = new ArrayList<>();
 
     // Additional constructor if you want to set content only
-    public SaigaiMessage(String content) {
+    public SagaiMessage(String content) {
         this.content = content;
         this.comments = new ArrayList<>(); // Ensure comments is initialized
     }
@@ -53,7 +53,7 @@ public class SaigaiMessage implements Comparable<SaigaiMessage> {
     }
 
     @Override
-    public int compareTo(SaigaiMessage other){
+    public int compareTo(SagaiMessage other){
         String prompt = createPrompt(this.content, other.content);
         return callGroqAPI(prompt);
        //return this.content.compareTo(other.content);
@@ -108,12 +108,12 @@ public class SaigaiMessage implements Comparable<SaigaiMessage> {
      /** 2nd telescoping method to create a Message object with parameterized comments
      * @param comments 
      */
-    public static SaigaiMessage createMessage(String content, String reply) {
-        SaigaiMessage message = new SaigaiMessage();
+    public static SagaiMessage createMessage(String content, String reply) {
+        SagaiMessage message = new SagaiMessage();
         message.setContent(content);
     
-        List<SaigaiComment> comments = new ArrayList<>();
-        SaigaiComment comment = new SaigaiComment();
+        List<SagaiComment> comments = new ArrayList<>();
+        SagaiComment comment = new SagaiComment();
         comment.setContent(reply);
         comments.add(comment);
         message.setComments(comments);
@@ -126,11 +126,11 @@ public class SaigaiMessage implements Comparable<SaigaiMessage> {
     /** Static method to initialize an array list of Message objects 
      * @return Message[], an array of Message objects
      */
-    public static SaigaiMessage[] init() {
-        ArrayList<SaigaiMessage> messages = new ArrayList<>();
+    public static SagaiMessage[] init() {
+        ArrayList<SagaiMessage> messages = new ArrayList<>();
         messages.add(createMessage("whats your favorite color", "red"));
         messages.add(createMessage("What is an object in java", "String"));
-        return messages.toArray(new SaigaiMessage[0]);
+        return messages.toArray(new SagaiMessage[0]);
     }
 
       /** Static method to print Message objects from an array
@@ -138,10 +138,10 @@ public class SaigaiMessage implements Comparable<SaigaiMessage> {
      */
     public static void main(String[] args) {
         // obtain Message from initializer
-        SaigaiMessage messages[] = init();
+        SagaiMessage messages[] = init();
 
         // iterate using "enhanced for loop"
-        for( SaigaiMessage message : messages) {
+        for( SagaiMessage message : messages) {
             System.out.println(message);  // print object
         }
     }
--- src/main/java/com/nighthawk/spring_portfolio/mvc/messages/SagaiMessageApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/messages/SagaiMessageApiController.java
@@ -12,19 +12,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController // annotation to simplify the creation of RESTful web services
-@RequestMapping("/api/saigai/messages")  // all requests in file begin with this URI
+@RequestMapping("/api/sagai/messages")  // all requests in file begin with this URI
 @CrossOrigin(origins = {"http://127.0.0.1:4100","https://nighthawkcoders.github.io/portfolio_2025/"}, allowCredentials = "true")
-public class SaigaiMessageApiController {
-     private static final Logger logger = LoggerFactory.getLogger(SaigaiMessageApiController.class);
+public class SagaiMessageApiController {
+     private static final Logger logger = LoggerFactory.getLogger(SagaiMessageApiController.class);
     // Autowired enables Control to connect URI request and POJO Object to easily for Database CRUD operations
     @Autowired
-    private SaigaiMessageJpaRepository messageRepository;
+    private SagaiMessageJpaRepository messageRepository;
 
     /* GET List of Jokes
      * @GetMapping annotation is used for mapping HTTP GET requests onto specific handler methods.
      */
     @GetMapping("/")
-    public ResponseEntity<List<SaigaiMessage>> getMessages() {
+    public ResponseEntity<List<SagaiMessage>> getMessages() {
         // ResponseEntity returns List of Jokes provide by JPA findAll()
         return new ResponseEntity<>( messageRepository.findAll(), HttpStatus.OK);
     }
@@ -33,20 +33,20 @@ public class SaigaiMessageApiController {
      * @PutMapping annotation is used for mapping HTTP PUT requests onto specific handler methods.
      * @PathVariable annotation extracts the templated part {id}, from the URI
      */
-    @PostMapping("/saigai/message")
-    public ResponseEntity<SaigaiMessage>  createMessage(@RequestBody SaigaiMessage message) {
+    @PostMapping("/sagai/message")
+    public ResponseEntity<SagaiMessage>  createMessage(@RequestBody SagaiMessage message) {
 
          if (message.getComments() == null) {
             message.setComments(new ArrayList<>());
         }
 
        
-        for (SaigaiMessage oldmessage :  messageRepository.findAll()) {
+        for (SagaiMessage oldmessage :  messageRepository.findAll()) {
             if(oldmessage.compareTo(message)== 1){
                 return ResponseEntity.status(409).body(oldmessage);
             }
         }
-        SaigaiMessage savedMessage = messageRepository.save(message);
+        SagaiMessage savedMessage = messageRepository.save(message);
         return ResponseEntity.status(201).body(savedMessage);
     }
 
@@ -62,7 +62,7 @@ public class SaigaiMessageApiController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<SaigaiMessage> getMessageById(@PathVariable Long id) {
+    public ResponseEntity<SagaiMessage> getMessageById(@PathVariable Long id) {
         return messageRepository.findById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
