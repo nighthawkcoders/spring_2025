f032d5aa3327c3205a15b31925c8a068b3da6a24
nitinsandiego <nitinsandiego@gmail.com>
2025-02-10 09:29:48 -0800

    Update StudentInfoJPARepository.java

--- src/main/java/com/nighthawk/spring_portfolio/mvc/student/StudentInfoJPARepository.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/student/StudentInfoJPARepository.java
@@ -51,5 +51,3 @@ public interface StudentInfoJPARepository extends JpaRepository<StudentInfo, Lon
     List<Object[]> findSelectedFieldsByPersonName(@Param("person_name") String person_name);
 
 }
-
-
