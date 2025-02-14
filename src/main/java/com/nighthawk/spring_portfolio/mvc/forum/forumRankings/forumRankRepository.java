009eaf9eaa61d33081f7884d939c491a46438a85
iKAN2025 <142475176+iKAN2025@users.noreply.github.com>
2025-02-14 08:21:14 -0800

    Revert "Forum"

--- src/main/java/com/nighthawk/spring_portfolio/mvc/forum/forumRankings/forumRankRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nighthawk.spring_portfolio.mvc.forum.forumRankings;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface forumRankRepository extends JpaRepository<forumRankings, Long> {
-    forumRankings findByAuthor(String author); // Custom query method to find a forumRankings by author
-}
