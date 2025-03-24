c2bdfb8cde87cf809628d5c2ce2e56f142b7391e
Joshua Thinh <joshua.thinh@gmail.com>
2025-03-24 00:21:19 -0700

    Add Api to get the Teacher Grade by id.

--- src/main/java/com/nighthawk/spring_portfolio/mvc/messages/TeacherGradingTeamTeachAPIController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/messages/TeacherGradingTeamTeachAPIController.java
@@ -1,14 +1,19 @@
 package com.nighthawk.spring_portfolio.mvc.messages;
 
+import java.util.Collections;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
+
+
+import jakarta.transaction.Transactional;
 
 
 /**
@@ -26,12 +31,35 @@ public class TeacherGradingTeamTeachAPIController {
      * Create a new Team Grade submit
      * 
      * @param teamGrade the AssignmentSubmission object to be created
-     * @return a ResponseEntity containing the created submission and HTTP status CREATED
+     * @return a ResponseEntity containing the TeacherGradingTeamTeach submission and HTTP status CREATED
      */
     @PostMapping("/submit")
     public ResponseEntity<TeacherGradingTeamTeach> createAssignment(@RequestBody TeacherGradingTeamTeach teamGrade) {
         teachgergGradingTeamTeachRepo.save(teamGrade);
         return new ResponseEntity<>(teamGrade, HttpStatus.CREATED);
     }
+
+    /**
+     *  get the grade for the team. 
+     * 
+     * @param TeacherGradingTeamTeachId the ID of the TeacherGradingTeamTeach to be fetch
+     * @return a ResponseEntity containing a TeacherGradingTeamTeach or an error 
+     */
+    @Transactional
+    @GetMapping("/teamgrade/{TeacherGradingTeamTeachId}")
+    public ResponseEntity<?> getTeacherGradeByAssignment(@PathVariable Long TeacherGradingTeamTeachId) {
+        TeacherGradingTeamTeach teamgrade = teachgergGradingTeamTeachRepo.findById(TeacherGradingTeamTeachId).orElse(null);
+        if (teamgrade == null) {
+            return new ResponseEntity<>(
+                Collections.singletonMap("error", "Assignment not found"), 
+                HttpStatus.NOT_FOUND
+            );
+        }
+        return new ResponseEntity<>(teamgrade, HttpStatus.OK);
+    }
+
+    
+
+
 
 }
