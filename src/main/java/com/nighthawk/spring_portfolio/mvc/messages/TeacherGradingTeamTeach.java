8a11af1dfb976f5c8515571de1a6a20850995721
Joshua Thinh <joshua.thinh@gmail.com>
2025-03-31 02:33:27 -0700

    working version integration with topic and students

--- src/main/java/com/nighthawk/spring_portfolio/mvc/messages/TeacherGradingTeamTeach.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/messages/TeacherGradingTeamTeach.java
@@ -3,10 +3,18 @@ package com.nighthawk.spring_portfolio.mvc.messages;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.nighthawk.spring_portfolio.mvc.teamteach.Topic;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,14 +28,21 @@ public class TeacherGradingTeamTeach {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    private String team;
+
+    @ManyToOne
+    @JoinColumn(name = "topic_id")
+    @JsonBackReference
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Topic topic;
+    private String student;
     private Double grade;
     private String comment;
     
-    public TeacherGradingTeamTeach(String team, Double grade, String comment) {
+    public TeacherGradingTeamTeach(Topic topic, String student, Double grade, String comment) {
         this.grade = grade;
+        this.student = student;
         this.comment = comment;
-        this.team = team;
+        this.topic = topic;
 
     }
 
@@ -36,8 +51,11 @@ public class TeacherGradingTeamTeach {
         return id;
     }
 
-    public String getTeam() {
-        return team;
+    public Topic getTopic() {
+        return topic;
+    }
+    public String getStudent() {
+        return student;
     }
 
     public String getComment() {
@@ -50,24 +68,25 @@ public class TeacherGradingTeamTeach {
 
          /** 
       * This method is an static message to create a Default Team Grade
-     * @param team the team
+     * @param topic the team
      * @param grade the grade
      * @param comment the comment of the grade
      * @return the TeacherGradingTeamTeach object;
      */
-    public static TeacherGradingTeamTeach createTeacherGrade(String team, Double grade, String comment) {
-        TeacherGradingTeamTeach teamgrade = new TeacherGradingTeamTeach(team,grade, comment);
+    public static TeacherGradingTeamTeach createTeacherGrade(Topic topic, String student, Double grade, String comment) {
+        TeacherGradingTeamTeach teamgrade = new TeacherGradingTeamTeach(topic,student, grade, comment);
         return teamgrade;
     }
+    
 
       /** Static method to initialize an array list of TeacherGradingTeamTeach objects 
      * @return TeacherGradingTeamTeach[], an array of TeacherGradingTeamTeach objects
      */
     public static TeacherGradingTeamTeach[] init() {
         ArrayList<TeacherGradingTeamTeach> teacherGradingTeamTeachs = new ArrayList<>();
-        teacherGradingTeamTeachs.add(createTeacherGrade("TeamA", 97.2, "Great job"));
-        teacherGradingTeamTeachs.add(createTeacherGrade("TeamB", 87.2, "Nice job"));
-        teacherGradingTeamTeachs.add(createTeacherGrade("TeamC", 77.2, "Good job"));
+        // teacherGradingTeamTeachs.add(createTeacherGrade(1, 97.2, "Great job"));
+        // teacherGradingTeamTeachs.add(createTeacherGrade(1, 87.2, "Nice job"));
+        // teacherGradingTeamTeachs.add(createTeacherGrade(1, 77.2, "Good job"));
         return teacherGradingTeamTeachs.toArray(new TeacherGradingTeamTeach[0]);
 
     }
