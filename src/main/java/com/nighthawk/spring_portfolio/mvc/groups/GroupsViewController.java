ec6091ba283c5437688128712b27a4364e2efe96
iwu78 <keledang777@gmail.com>
2025-04-28 17:58:00 -0700

    allow users to create groups, queue display update

--- src/main/java/com/nighthawk/spring_portfolio/mvc/groups/GroupsViewController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/groups/GroupsViewController.java
@@ -9,26 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.server.ResponseStatusException;
 
-import com.nighthawk.spring_portfolio.mvc.person.Person;
-import com.nighthawk.spring_portfolio.mvc.person.PersonJpaRepository;
-
 @Controller
 @RequestMapping("/mvc/groups")
 public class GroupsViewController {
-    @Autowired
-    private PersonJpaRepository personRepository;
-
     @GetMapping("/group-tracker")
     public String assignmentTracker(Model model, @AuthenticationPrincipal UserDetails userDetails) {
-        String uid = userDetails.getUsername();
-        Person user = personRepository.findByUid(uid);
-        
-        if (user == null || (!user.hasRoleWithName("ROLE_TEACHER") && !user.hasRoleWithName("ROLE_ADMIN"))) {
-            throw new ResponseStatusException(
-                HttpStatus.FORBIDDEN, "You must be a teacher or admin to access the group tracker"
-            );
-        }
-
         return "group/group";
     }
 }
