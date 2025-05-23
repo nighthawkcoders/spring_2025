183ccf15a225a22e09a6bb2ed2c638a7279dc7ca
iKAN2025 <142475176+iKAN2025@users.noreply.github.com>
2025-05-23 11:29:00 -0700

    update bank

--- src/main/java/com/nighthawk/spring_portfolio/mvc/bank/BankJpaRepository.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/bank/BankJpaRepository.java
@@ -9,6 +9,7 @@ public interface BankJpaRepository extends JpaRepository<Bank, Long> {
     Bank findByPersonId(Long personId);
     Bank findByUsername(String username);
     Bank findByUid(String uid);    
+    List<Bank>  findByUidContainingIgnoreCase(String uid);
     // Find top 10 banks ordered by balance in descending order (for leaderboard)
     List<Bank> findTop10ByOrderByBalanceDesc();
     @Query("SELECT p FROM Bank p ORDER BY CAST(p.balance AS double) DESC LIMIT 5")
